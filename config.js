b279238bf22aacc0187d337955508a335e0953c4
johan <johanribl@gmail.com>
2025-03-04 09:23:25 +0100

    a

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: 'acces:AKIAY6QVZPUINYFO5FEZ',
-    secretAccessKey: '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',
-    endpoint: 'https://esgi-cloud-exam-bucket-47798.s3.eu-west-1.amazonaws.com',
-    bucket: 'esgi-cloud-exam-bucket-47798',
+    accessKeyId: 'AKIAY6QVZPUIIR2ACA5V',
+    secretAccessKey: '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',
+    endpoint: 's3.eu-west-1.amazonaws.com',
+    bucket: 'esgi-cloud-exam-bucket-47798'
   },
   suffix: {
     small: '_small.jpg',
